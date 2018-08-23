ba44536a52583c655a841d6ef8a8163035b4a8b5
唯ID <liangtb@qq.com>
2018-08-23 19:02:15 +0800

    sdk优化后更新demo；精简demo 删掉不必要文件；更新readme；其他小改动

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
-## 下载
-git clone https://gitee.com/lltb/onlyid-demo-android.git OnlyID_Demo
+## 运行说明
+* git clone https://gitee.com/lltb/onlyid-demo-android.git OnlyID_Demo
+* 使用Android Studio（3.0或以上）打开，运行 app 模块
+* 点击 手机号登录 开始授权流程
 
-## 简介
-net.onlyid.onlyid_demo.MainActivity\
-演示接入流程
-
-net.onlyid.onlyid_demo.MyHttp\
-封装请求网络
--- app/src/main/java/net/onlyid/onlyid_demo/MainActivity.java
+++ app/src/main/java/net/onlyid/onlyid_demo/MainActivity.java
@@ -1,102 +1,75 @@
 package net.onlyid.onlyid_demo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import net.onlyid.onlyid_sdk.OnlyID;
+import net.onlyid.onlyid_sdk.AuthActivity;
 
 import java.io.IOException;
 
 import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
+import okhttp3.Response;
+import okhttp3.ResponseBody;
 
-public class MainActivity extends Activity implements OnlyID.AuthListener {
-    static final String CLIENT_ID = "5adac916904be93f3f621003", CLIENT_SECRET = "f71c0c12997a00cf95fe82e130a45711";
-    // 你的服务端地址
-    static final String MY_URL = "https://demo.onlyid.net/";
-    private static final String TAG = "OnlyID_Demo";
-    TextView textView, textView2, textView3;
-    boolean clientTokenFlow = true;
+public class MainActivity extends Activity {
+    TextView tip, tip1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textView = findViewById(R.id.textView);
-        textView2 = findViewById(R.id.textView2);
-        textView3 = findViewById(R.id.textView3);
-
-        MyHttp.initOkHttpClient(this);
+        tip = findViewById(R.id.tip);
+        tip1 = findViewById(R.id.tip1);
     }
 
-    /**
-     * 演示客户端token流程 适合大多数开发者
-     * @param view
-     */
-    public void auth(View view) {
-        // 最后两字段为自定义选项 请参看文档查看用法 或直接传null
-        OnlyID.auth(this, CLIENT_ID, null, this, CLIENT_SECRET, null, null);
-    }
-
-    /**
-     * 演示服务端中转流程 适合安全性要求高的开发者
-     * @param view
-     */
-    public void auth1(View view) {
-        clientTokenFlow = false;
-        OnlyID.auth(this, CLIENT_ID, null, this, null, null, null);
+    public void login(View view) {
+        Intent intent = new Intent(this, AuthActivity.class);
+        intent.putExtra("clientId", "5adac916904be93f3f621003");
+        startActivityForResult(intent, 1);
     }
 
     @Override
-    public void onAuthResponse(OnlyID.AuthResponse authResponse) {
-        Log.d(TAG, "onAuthResponse: " + authResponse);
-        textView2.setText("SDK返回：" + authResponse.toString());
-        if (authResponse.code == OnlyID.ErrCode.CANCEL) {
-            // do nothing
-            return;
-        }
-        if (authResponse.code == OnlyID.ErrCode.NETWORK_ERR) {
-            Toast.makeText(this, "网络错误, 请重试", Toast.LENGTH_LONG).show();
-            return;
-        }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode != 1) return;
 
-        // 使用客户端token流程时 sdk直接返回accessToken
-        if (clientTokenFlow) {
-            RequestBody requestBody = new FormBody.Builder().add("accessToken", authResponse.accessToken).build();
-            Request request = new Request.Builder().url(MY_URL + "auth-app").post(requestBody).build();
-            MyHttp.request(request, new MyHttp.MyCallback() {
-                @Override
-                public void onFailure(Call call, IOException e, Integer code) {
-                    textView3.setText("请求错误：" + e.toString());
-                }
+        if (resultCode == RESULT_CANCELED) { tip.setText("用户取消"); return; }
 
-                @Override
-                public void onSuccess(Call call, String s) {
-                    textView3.setText("服务端返回：" + s);
-                }
-            });
-        }
-        // 使用服务端中转流程时 sdk返回authCode
-        else {
-            RequestBody requestBody = new FormBody.Builder().add("code", authResponse.authCode).build();
-            Request request = new Request.Builder().url(MY_URL + "auth-app1").post(requestBody).build();
-            MyHttp.request(request, new MyHttp.MyCallback() {
-                @Override
-                public void onFailure(Call call, IOException e, Integer code) {
-                    textView3.setText("请求错误：" + e.toString());
-                }
+        if (resultCode == AuthActivity.RESULT_NETWORK_ERR) { tip.setText("网络错误"); return; }
 
-                @Override
-                public void onSuccess(Call call, String s) {
-                    textView3.setText("服务端返回：" + s);
+        String code = data.getStringExtra("code");
+
+        // 生产环境使用时，获取用户信息建议在服务端进行，以防泄露你的client secret
+        RequestBody requestBody = new FormBody.Builder().add("code", code)
+                .add("client_id", "5adac916904be93f3f621003")
+                .add("client_secret", "f71c0c12997a00cf95fe82e130a45711").build();
+        Request request = new Request.Builder().url("https://my.onlyid.net/user").post(requestBody).build();
+        new OkHttpClient().newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, final IOException e) {
+                runOnUiThread(() -> { tip.setText("网络错误：\n" + e); });
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try (ResponseBody responseBody = response.body()) {
+                    final String res = responseBody.string();
+                    if (!response.isSuccessful()) {
+                        runOnUiThread(() -> { tip.setText("登录失败，错误信息：\n" + res); });
+                        return;
+                    }
+                    runOnUiThread(() -> { tip.setText("登录成功，用户信息：\n" + res); });
                 }
-            });
-        }
+            }
+        });
+        tip1.setText("再试一次：");
     }
 }
